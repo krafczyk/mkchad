299da4dc626b3761dd490f78493c22e366e2c94d
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2024-12-16 22:53:22 -0600

    Add cursor highlights, and treesitter syntax highlighting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,17 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+    config = function()
+      require "configs.nvim-treesitter"
+    end,
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "c", "python",
+        "bash"
+  		},
+  	},
+  },
 }
