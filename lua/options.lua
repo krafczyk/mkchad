76098cd3ba885b75f2052c6bbc6acff13a5d82e2
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2024-12-21 01:22:35 -0600

    Add a few new options

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,16 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Reserve a space in the gutter
+--vim.opt.signcolumn = 'yes'
+
+-- Keep the cursor in the middle of the screen
+o.so = 999
+
+-- Add quick option for repeating previous command buffer action
+vim.api.nvim_set_keymap('n', ',', '@@', { noremap = false, silent = false })
+
+-- How I like searching to be done
+o.incsearch = true
+o.hlsearch = true
