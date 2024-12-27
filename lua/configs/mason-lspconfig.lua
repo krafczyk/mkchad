32159ef8370213b714e190e35cd9a07b27e63bc1
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2024-12-26 21:27:37 -0600

    Change pyright to basedpyright

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 -- require("lspconfig").setup({})
 require("mason-lspconfig").setup({
     automatic_installation = true,
-    ensure_installed = { "lua_ls", "pyright", "clangd", "jdtls" }
+    ensure_installed = { "lua_ls", "basedpyright", "clangd", "jdtls" }
 })
 
 local map = vim.keymap.set
