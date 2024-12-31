b2f8507bf2ef23b5075c304c026055ab725aa3e2
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2024-12-31 13:17:08 -0600

    Add typescript language server for default installation.

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 -- require("lspconfig").setup({})
 require("mason-lspconfig").setup({
     automatic_installation = true,
-    ensure_installed = { "lua_ls", "basedpyright", "clangd", "jdtls" }
+    ensure_installed = { "lua_ls", "basedpyright", "clangd", "jdtls", "typescript-language-server" }
 })
 
 local map = vim.keymap.set
