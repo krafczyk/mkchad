b2551e4bbdf6b58c551fb75bfb7a105176942721
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-01-04 17:04:01 -0600

    Add new bindings for lsp functionality

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,22 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+
+-- Hover Diagnostic Command
+map("n", "<leader>d", vim.diagnostic.open_float)
+
+-- Navigate location list
+map("n", "<leader>ln", ":lnext<CR>", { silent = true, desc = "Go to next location in location list" })
+map("n", "<leader>lp", ":lprev<CR>", { silent = true, desc = "Go to next location in location list" })
+
+-- Clear location list
+map("n", "<leader>cl", function()
+  vim.fn.setloclist(0, {})
+end, { silent = true, desc = "Clear location list"})
+
+-- Populate location list with diagnostics
+map("n", "<leader>pl", function()
+  vim.diagnostic.setloclist()
+end, { silent = true, desc = "Populate location list"})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
