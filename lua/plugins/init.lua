521d41dd6b726fca9b8c6aa1a23bb381f58cd7d7
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-01-07 11:05:55 -0600

    Trying out noice.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,22 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    --opts = {},
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify"
+    },
+    config = function()
+      require "configs.noice"
+    end
+  },
   -- Basic plugins which don't need anything special
   {
     "tpope/vim-commentary",
