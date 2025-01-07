b210bf89a3fa7c8d410d31e130fe65b568c31b0e
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-01-07 17:18:09 -0600

    Change cmdline notifications so they're on the bottom

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -14,5 +14,17 @@ require('noice').setup({
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+  views = {
+    cmdline_popup = {
+      position = {
+        row = -2, -- Negative value positions it relative to the bottom
+        col = 0, -- CMDline appears on the bottom left. Centered horizontally (50%)
+      },
+      size = {
+        width = "auto",
+        height = "auto",
+      }
+    },
   }
 })
