e7ad34a3b870865e9cf4eeb9c126d538fac6a87f
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-01-16 01:32:38 -0600

    Large edit to allow greater control over copilot sessions.

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -4,29 +4,46 @@ local lspkind = require('lspkind')
 
 local cmp_enabled = true
 
--- Function to toggle nvim-cmp
-function ToggleCmp()
-    cmp_enabled = not cmp_enabled
-    if cmp_enabled then
-        print("nvim-cmp enabled")
-    else
-        print("nvim-cmp disabled")
-    end
-end
+-- Copilot behavior customization section
+local CopilotMode = {
+  DISABLED = 'disabled',
+  ENABLED = 'enabled',
+  ALWAYS = 'always',
+}
 
--- Copilot enabled state
-local copilot_enabled = false
+local CopilotState = {
+  session_copilot_allow = false,
+  session_copilot_default = CopilotMode.DISABLED,
+}
 
--- Function to toggle Copilot for the entire session
-local function toggle_copilot_session(enable)
-  copilot_enabled = enable
-end
+vim.api.nvim_create_user_command('CopilotReportManagerState', function()
+  -- Print the current state of the Copilot manager
+  local msg = "Copilot Manager State:\n"
+  for k, v in pairs(CopilotState) do
+    msg = msg .. k .. ": " .. tostring(v) .. "\n"
+  end
+  msg = msg .. "vim.b.copilot_mode: " .. tostring(vim.b.copilot_mode) .. "\n"
+  print(msg)
+end, { desc = "Print the current state of the Copilot manager." })
 
--- Function to toggle Copilot for the current buffer
-local function toggle_copilot_buffer(enable)
-  vim.b.copilot_enabled = enable
-end
+local cmp_sources_wo_copilot = {
+  { name = "nvim_lsp" },
+  { name = "luasnip" },
+  { name = "buffer" },
+  { name = "nvim_lua" },
+  { name = "async_path" },
+}
 
+local cmp_sources_w_copilot = {
+  { name = "copilot" , priority = 100 }, -- Unsure if this is the right priority level
+  { name = "nvim_lsp" },
+  { name = "luasnip" },
+  { name = "buffer" },
+  { name = "nvim_lua" },
+  { name = "async_path" },
+}
+
+-- Initialization of nvim-cmp
 cmp.setup {
   enabled = function()
     return cmp_enabled
@@ -34,7 +51,7 @@ cmp.setup {
   snippet = {
     expand = function(args)
       -- vim.snippet.expand(args.body) -- For native neovim snippets
-      require('luasnip').lsp_expand(args.body) -- For LuaSnip users
+      luasnip.lsp_expand(args.body) -- For LuaSnip users
     end,
   },
   window = {
@@ -80,13 +97,7 @@ cmp.setup {
       end
     end, { "i", "s" }),
   }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "nvim_lua" },
-    { name = "async_path" },
-  }),
+  sources = cmp.config.sources(cmp_sources_wo_copilot),
   matching = {
     disallow_fuzzy_matching = false,
   },
@@ -107,57 +118,193 @@ cmp.setup {
   },
 }
 
+-- Function to toggle nvim-cmp
+function ToggleCmp()
+    cmp_enabled = not cmp_enabled
+    if cmp_enabled then
+        print("nvim-cmp enabled")
+    else
+        print("nvim-cmp disabled")
+    end
+end
+
 -- Keymap to toggle cmp
 vim.api.nvim_set_keymap('n', '<leader>ct', ':lua ToggleCmp()<CR>', { noremap = true, silent = true })
 
--- Command to manually request Copilot completion
-function _G.trigger_copilot_complete()
-  -- Check if Copilot is enabled (globally or for the buffer)
-  if not copilot_enabled and not vim.b.copilot_enabled then
-    print("Copilot is not enabled for this session or buffer.")
-    return
+-- Method to update cmp sources given the current Copilot state
+local function set_cmp_sources(copilot)
+  local cur_cmp = require('cmp')
+  cur_cmp.abort() -- Aborting completion session
+  if copilot then
+    cur_cmp.setup({
+      sources = cmp_sources_w_copilot
+    })
+  else
+    cur_cmp.setup({
+      sources = cmp_sources_wo_copilot
+    })
   end
-
-  -- Modify cmp config for one completion
-  cmp.abort()
-  local config = require('cmp.config').get()
-  local sources_copy = vim.deepcopy(config.sources)
-  table.insert(sources_copy, 1, { group_index = 0, name = "copilot", option = {} })
-  cmp.complete({ config = { sources = sources_copy } })
 end
 
-vim.api.nvim_set_keymap(
-  'i', -- Insert mode
-  '<C-c>', -- Replace <C-c> with your preferred key combination
-  [[<Cmd>lua trigger_copilot_complete()<CR>]],
-  { noremap = true, silent = true }
-)
+local function update_cmp_sources()
+  -- Check whether copilot is allowed for the session
+  if not CopilotState.session_copilot_allow then
+    set_cmp_sources(false)
+  end
 
--- vim.api.nvim_create_user_command('CopilotComplete', function()
---   local original_sources = cmp.get_config().sources
---   local sources = vim.deepcopy(original_sources) -- Create a shallow copy of the sources
---   table.insert(sources, 1, { name = "copilot" }) -- Add Copilot as the first source
+  -- Check whether a buffer has variable tracking its copilot mode
+  if vim.b.copilot_mode == nil then
+    vim.b.copilot_mode = CopilotState.session_copilot_default
+  end
 
---   cmp.complete({ config = { sources = sources } }) -- Trigger completion with modified sources
--- end, {})
+  -- Otherwise we only set copilot as a source if it is on 'ALWAYS' mode.
+  if vim.b.copilot_mode == CopilotMode.DISABLED or vim.b.copilot_mode == CopilotMode.ENABLED then
+    set_cmp_sources(false)
+  else
+    set_cmp_sources(true)
+  end
+end
 
--- Commands to toggle Copilot
-vim.api.nvim_create_user_command('EnableCopilotSession', function()
-  toggle_copilot_session(true)
-  print("Copilot enabled for this session.")
-end, {})
+-- Helper function for protecting accidental enablement.
+local function copilot_session_enable(prompt, quiet)
+  if not CopilotState.session_copilot_allow then
+    if prompt then
+      vim.ui.input({ prompt = "Copilot is globally disabled. Are you sure you want to enable it? (y/n): " }, function(input)
+        if input and string.lower(input) == "y" then
+          CopilotState.session_copilot_allow = true
+          if not quiet then
+            print("Copilot globally enabled.")
+          end
+        else
+          if not quiet then
+            print("Operation canceled. Copilot remains globally disabled.")
+          end
+        end
+      end)
+    end
+  else
+    if not quiet then
+      print("Copilot is already globally enabled.")
+    end
+  end
+  update_cmp_sources()
+end
 
-vim.api.nvim_create_user_command('DisableCopilotSession', function()
-  toggle_copilot_session(false)
-  print("Copilot disabled for this session.")
-end, {})
+-- Commands to enable/disable the session
+vim.api.nvim_create_user_command('CopilotSessionEnable', function()
+  copilot_session_enable(true, false)
+end, { desc = "Enable copilot globally for the current session."})
 
-vim.api.nvim_create_user_command('EnableCopilotBuffer', function()
-  toggle_copilot_buffer(true)
-  print("Copilot enabled for this buffer.")
-end, {})
+vim.api.nvim_create_user_command('CopilotSessionDisable', function()
+  if not CopilotState.session_copilot_allow then
+    print("Copilot already disabled globally.")
+  else
+    CopilotState.session_copilot_allow = false
+    print("Copilot now disabled globally.")
+  end
+  update_cmp_sources()
+end, { desc = "Disable copilot globally for the current session."})
 
-vim.api.nvim_create_user_command('DisableCopilotBuffer', function()
-  toggle_copilot_buffer(false)
-  print("Copilot disabled for this buffer.")
-end, {})
+vim.api.nvim_create_user_command('CopilotSessionOptIn', function()
+  copilot_session_enable(true, false)
+  CopilotState.session_copilot_default = CopilotMode.DISABLED
+end, { desc = "Set current session to opt-in for copilot." })
+
+vim.api.nvim_create_user_command('CopilotSessionOptOut', function()
+  copilot_session_enable(true, false)
+  CopilotState.session_copilot_default = CopilotMode.ALWAYS
+end, { desc = "Set the current session to opt-out for copilot." })
+
+vim.api.nvim_create_user_command('CopilotBufferEnable', function()
+  if not CopilotState.session_copilot_allow then
+    print("Cannot enable. Copilot is currently disabled globally.")
+  else
+    if CopilotState.session_copilot_default == CopilotMode.DISABLED and vim.b.copilot_mode == CopilotMode.DISABLED then
+      vim.ui.input({ prompt = "Copilot is currently Opt In. Are you sure you want to enable it for this buffer? (y/n): "}, function(input)
+        if input and string.lower(input) == "y" then
+          vim.b.copilot_mode = CopilotMode.ENABLED
+          print("Copilot enabled for this buffer.")
+        else
+          print("Operation canceled. Copilot remains disabled for this buffer.")
+        end
+      end)
+    else
+      vim.b.copilot_mode = CopilotState.session_copilot_default
+      print("Copilot enabled for this buffer.")
+    end
+  end
+  update_cmp_sources()
+end, { desc = "Enable copilot for the current buffer." })
+
+vim.api.nvim_create_user_command('CopilotBufferSetEnable', function()
+  if not CopilotState.session_copilot_allow then
+    print("Cannot enable. Copilot is currently disabled globally.")
+  else
+    if CopilotState.session_copilot_default == CopilotMode.DISABLED and vim.b.copilot_mode == CopilotMode.DISABLED then
+      vim.ui.input({ prompt = "Copilot is currently Opt In. Are you sure you want to enable it for this buffer? (y/n): "}, function(input)
+        if input and string.lower(input) == "y" then
+          vim.b.copilot_mode = CopilotMode.ENABLED
+          print("Copilot enabled for this buffer.")
+        else
+          print("Operation canceled. Copilot remains disabled for this buffer.")
+        end
+      end)
+    else
+      vim.b.copilot_mode = CopilotMode.ENABLED
+      print("Copilot enabled for this buffer.")
+    end
+  end
+  update_cmp_sources()
+end, { desc = "Set copilot mode to ENABLED for the current buffer." })
+
+
+vim.api.nvim_create_user_command('CopilotBufferAlways', function()
+  if not CopilotState.session_copilot_allow then
+    print("Cannot enable. Copilot is currently disabled globally.")
+  else
+    if CopilotState.session_copilot_default == CopilotMode.DISABLED and vim.b.copilot_mode == CopilotMode.DISABLED then
+      vim.ui.input({ prompt = "Copilot is currently Opt In. Are you sure you want to enable it for this buffer? (y/n): "}, function(input)
+        if input and string.lower(input) == "y" then
+          vim.b.copilot_mode = CopilotMode.ALWAYS
+          print("Copilot enabled for this buffer.")
+        else
+          print("Operation canceled. Copilot remains disabled for this buffer.")
+        end
+      end)
+    else
+      vim.b.copilot_mode = CopilotMode.ALWAYS
+      print("Copilot enabled for this buffer.")
+    end
+  end
+  update_cmp_sources()
+end, { desc = "Turn copilot on all the time for the current buffer." })
+
+vim.api.nvim_create_user_command('CopilotBufferDisable', function()
+  if not CopilotState.session_copilot_allow then
+    print("No need to do anything. Copilot is currently disabled globally.")
+  else
+    vim.b.copilot_mode = CopilotMode.DISABLED
+    print("Copilot disabled for this buffer.")
+  end
+  update_cmp_sources()
+end, { desc = "Disable copilot for the current buffer." })
+
+-- Command to manually request Copilot completion
+local function trigger_copilot_complete()
+  -- Check if Copilot is enabled (globally or for the buffer)
+  if not CopilotState.session_copilot_allow or vim.b.copilot_mode == CopilotMode.DISABLED then
+    print("Copilot is not enabled for this session or buffer.")
+  end
+
+  -- Request a new completion
+  local cur_cmp = require('cmp')
+  cur_cmp.abort()
+  cmp.complete({
+    config = {
+      sources = cmp_sources_w_copilot,
+    },
+    reason = cmp.ContextReason.Manual,
+  })
+end
+
+vim.keymap.set('i', '<C-c>', trigger_copilot_complete, { noremap = true, silent = true, desc = "Trigger a completion with copilot"})
