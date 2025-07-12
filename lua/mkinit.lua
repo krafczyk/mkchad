34a34c8b49674e967d2023df577b81177b54aee3
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-07-12 00:20:35 -0500

    Update how vims various files are handled

--- lua/mkinit.lua
+++ lua/mkinit.lua
@@ -16,3 +16,16 @@ augroup numbertoggle
   autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
 augroup END
 ]], false)
+
+-- Define swap/backup/undo file behavior
+local state = vim.fn.stdpath("state")
+local cache = vim.fn.stdpath("cache")
+
+vim.opt.backup = true
+vim.opt.writebackup = true -- keep a safety net
+vim.opt.swapfile = true
+vim.opt.undofile = true -- persistent undo
+
+vim.opt.backupdir = state .. "/backup//" -- double // = auto-mkdir + file shrub
+vim.opt.directory = state .. "/swap//"
+vim.opt.undodir = cache .. "/undo//"
